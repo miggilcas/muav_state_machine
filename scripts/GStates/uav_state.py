d3f4730e3583e36400aeb5a0fb15693543990cac
miggilcas <miguel.gil2000@gmail.com>
2023-05-04 12:04:24 +0200

    uav_state.py UAVState_msg defined and initialized

--- scripts/GStates/uav_state.py
+++ scripts/GStates/uav_state.py
@@ -9,18 +9,24 @@ from mavros_msgs.msg import ExtendedState
 # import custom message:
 from muav_state_machine.msg import UAVState
 
+# global variables to catch the data from the agent state machine node
+autopilot = String() 
+mission_state = String()
+wp_reached = UInt8()
+extended_state = ExtendedState()
+
+#callback functions
 def autopilot_cb(msg):
-    #rospy.loginfo(CGREEN+"waypoint reached: {}".format(msg.wp_seq)+CEND)
-    return msg
+    autopilot = msg
+
 def mission_state_cb(msg):
-    #rospy.loginfo(CGREEN+"waypoint reached: {}".format(msg.wp_seq)+CEND)
-    return msg
+    mission_state = msg
+
 def wp_reached_cb(msg):
-    #rospy.loginfo(CGREEN+"waypoint reached: {}".format(msg.wp_seq)+CEND)
-    return msg
+    wp_reached = msg
+
 def estate_cb(msg):
-    #rospy.loginfo(CGREEN+"waypoint reached: {}".format(msg.wp_seq)+CEND)
-    return msg
+    extended_state = msg
 
 class UavState(smach.State):
     def __init__(self,uav_id):#modify, common_data
@@ -49,27 +55,21 @@ class UavState(smach.State):
         # transition to X state
         while not rospy.is_shutdown():           
             # TBD: error detection if not namespaces with the name of the uav_id 
-            # catching the data from the agent state machine node
-            autopilot = autopilot_cb
-            mission_state = mission_state_cb
-            # catching the data from the agent state machine node
-            #mission_state = rospy.wait_for_message("/uav_{}_sm/com/mission_state".format(self.uav_id), String)
-            wp_reached = wp_reached_cb
-            extended_state = estate_cb
+            
 
             rospy.loginfo('[UavState] - UAV{} state: autopilot: {}, mission_state: {}, wp_reached: {}, extended_state: {}'.format(self.uav_id,autopilot,mission_state,wp_reached,extended_state))
             
             #fill the UAVState_msg custom message
-            # UAVState_msg.autopilot = autopilot #parameter
-            # UAVState_msg.mission_state = mission_state#published by the agent state machine
-            # if UAVState_msg.autopilot=="px4":
-            #     UAVState_msg.wp_reached = wp_reached #published by the agent state machine
-            #     UAVState_msg.extended_state = extended_state #published by the agent state machine
-            # else:
-            #     UAVState_msg.wp_reached = 0
-            # #     #UAVState_msg.extended_state = 0
-            # UAVState_msg.wp_reached = 0
-            # UAVState_msg.extended_state.vtol = 
+            UAVState_msg.autopilot = autopilot #parameter
+            UAVState_msg.mission_state = mission_state#published by the agent state machine
+            if UAVState_msg.autopilot=="px4" and mission_state=="mission_running":
+                UAVState_msg.wp_reached = wp_reached #published by the agent state machine
+                UAVState_msg.extended_state = extended_state #published by the agent state machine
+            else:
+                UAVState_msg.wp_reached = 0
+                UAVState_msg.extended_state.vtol_state = 0
+                UAVState_msg.extended_state.landed_state = 0
+            
 
             #publish the UAVState_msg custom message
             UAVState_pub.publish(UAVState_msg) 
